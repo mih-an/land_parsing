d2b24968747638daf354bbea55951b8fe60f7180
Andrey Mihaylov <trackclimb@gmail.com>
2024-01-09 14:14:59 +0300

    Refactoring: creating one common method for both Name and Favorite Wine scratching

--- html_readers/regexp_reader.py
+++ html_readers/regexp_reader.py
@@ -3,28 +3,30 @@ import re
 
 class HtmlReader:
     @staticmethod
-    def get_title(test_html):
+    def get_title(html_text):
         pattern = "<title.*?>.*?</title.*?>"
-        match_results = re.search(pattern, test_html, re.IGNORECASE)
+        match_results = re.search(pattern, html_text, re.IGNORECASE)
         title = match_results.group()
         # Remove HTML tags
         title = re.sub("<.*?>", "", title)
         return title
 
     @staticmethod
-    def get_name(test_html):
-        pattern = "Name:.*?<"
-        match_results = re.search(pattern, test_html, re.IGNORECASE)
-        name = match_results.group()
-        # Remove HTML tags and Name:
-        name = re.sub("<.*?", "", name)
-        name = re.sub("Name: ", "", name)
-        return name
+    def get_name(html_text):
+        return HtmlReader.get_item(html_text, "Name: ")
 
     @staticmethod
-    def get_fav_color(test_html):
-        pattern = "Favorite Color:.*"
-        match_results = re.search(pattern, test_html, re.IGNORECASE)
-        fav_wine = match_results.group()
-        fav_wine = re.sub("Favorite Color: ", "", fav_wine)
-        return fav_wine
+    def get_fav_color(html_text):
+        return HtmlReader.get_item(html_text, "Favorite Color:")
+
+    @staticmethod
+    def get_item(html_text, search_text):
+        string_start_idx = html_text.find(search_text)
+        text_start_idx = string_start_idx + len(search_text)
+
+        next_html_tag_offset = html_text[text_start_idx:].find("<")
+        text_end_idx = text_start_idx + next_html_tag_offset
+
+        raw_text = html_text[text_start_idx: text_end_idx]
+        clean_text = raw_text.strip(" \r\n\t")
+        return clean_text
