9ced8f447ec16f4e3548884bd746fe845a6de2ec
Andrey Mihaylov <trackclimb@gmail.com>
2024-01-09 13:59:50 +0300

    Getting Name and Favorite Wine added

--- html_readers/regexp_reader.py
+++ html_readers/regexp_reader.py
@@ -3,14 +3,6 @@ import re
 
 class HtmlReader:
     @staticmethod
-    def get_simple_title(html):
-        title_index = html.find("<title>")
-        start_index = title_index + len("<title>")
-        end_index = html.find("</title>")
-        title = html[start_index:end_index]
-        return title
-
-    @staticmethod
     def get_title(test_html):
         pattern = "<title.*?>.*?</title.*?>"
         match_results = re.search(pattern, test_html, re.IGNORECASE)
@@ -18,3 +10,21 @@ class HtmlReader:
         # Remove HTML tags
         title = re.sub("<.*?>", "", title)
         return title
+
+    @staticmethod
+    def get_name(test_html):
+        pattern = "Name:.*?<"
+        match_results = re.search(pattern, test_html, re.IGNORECASE)
+        name = match_results.group()
+        # Remove HTML tags and Name:
+        name = re.sub("<.*?", "", name)
+        name = re.sub("Name: ", "", name)
+        return name
+
+    @staticmethod
+    def get_fav_color(test_html):
+        pattern = "Favorite Color:.*"
+        match_results = re.search(pattern, test_html, re.IGNORECASE)
+        fav_wine = match_results.group()
+        fav_wine = re.sub("Favorite Color: ", "", fav_wine)
+        return fav_wine
--- html_readers/test_html_reader.py
+++ html_readers/test_html_reader.py
@@ -5,11 +5,22 @@ from html_readers.regexp_reader import HtmlReader
 
 class TestCaseGettingHtmlData(unittest.TestCase):
     def test_getting_html_title(self):
-        html_reader = HtmlReader()
-        with open('test_bad_title.html', 'r') as test_html_file:
+        with open('test_html.html', 'r') as test_html_file:
             test_html = test_html_file.read()
-        html_title = html_reader.get_title(test_html)
+        html_title = HtmlReader().get_title(test_html)
         self.assertEqual("Profile: Dionysus", html_title)
+
+    def test_getting_name(self):
+        with open('test_html.html', 'r') as test_html_file:
+            test_html = test_html_file.read()
+        name = HtmlReader().get_name(test_html)
+        self.assertEqual("Dionysus", name)
+
+    def test_getting_favorite_color(self):
+        with open('test_html.html', 'r') as test_html_file:
+            test_html = test_html_file.read()
+        fav_color = HtmlReader().get_fav_color(test_html)
+        self.assertEqual("Wine", fav_color)
 
 
 if __name__ == '__main__':
