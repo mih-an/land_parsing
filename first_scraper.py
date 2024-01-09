80929de07b254f0fe5cf4b97bd08fb1e6831ddbb
Andrey Mihaylov <trackclimb@gmail.com>
2024-01-09 11:34:02 +0300

    Get title added

--- first_scraper.py
+++ first_scraper.py
@@ -3,6 +3,8 @@ from urllib.request import urlopen
 from loaders.simple_reader import SimpleReader
 
 url = "http://olympus.realpython.org/profiles/aphrodite"
-html = SimpleReader().read_url(url)
+simple_reader = SimpleReader()
+html = simple_reader.read_url(url)
+title = simple_reader.get_title(html)
 
-print(html)
+print(title)
--- loaders/simple_reader.py
+++ loaders/simple_reader.py
@@ -9,3 +9,10 @@ class SimpleReader:
         html_bytes = page.read()
         html = html_bytes.decode("utf-8")
         return html
+
+    def get_title(self, html):
+        title_index = html.find("<title>")
+        start_index = title_index + len("<title>")
+        end_index = html.find("</title>")
+        title = html[start_index:end_index]
+        return title
