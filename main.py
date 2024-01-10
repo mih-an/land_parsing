b92757398e81fd4b0c1e6c2cbf60cae0faae4d86
Andrey Mihaylov <trackclimb@gmail.com>
2024-01-10 14:47:47 +0300

    Getting links using BeautifulSoup

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import mechanicalsoup
+
+# 1
 browser = mechanicalsoup.Browser()
-# url = "http://olympus.realpython.org/login"
-url = "https://www.cian.ru/cat.php?deal_type=sale&engine_version=2&object_type%5B0%5D=3&offer_type=suburban&p=2&region=4593"
-page = browser.get(url)
-print(page.soup)
+url = "https://www.avito.ru/moskva_i_mo/zemelnye_uchastki/prodam/izhs-ASgBAQICAUSWA9oQAUCmCBTmVQ?p=2"
+login_page = browser.get(url)
+login_html = login_page.soup
+print(login_html)
+
+# # 2
+# form = login_html.select("form")[0]
+# form.select("input")[0]["value"] = "zeus"
+# form.select("input")[1]["value"] = "ThunderDude"
+#
+# # 3
+# profiles_page = browser.submit(form, login_page.url)
+# print(profiles_page.soup.title)
