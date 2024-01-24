aa9792ce8582809a0b702ad795e5f26f70dbb4a7
Andrey Mihaylov <trackclimb@gmail.com>
2024-01-24 16:28:36 +0300

    Main program. Getting all sectors to parse

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ credentials_file = 'tests/test_data/google_creds.json'
 
 sl = SectorListLoader()
 sectors = sl.load_sectors(sheets_id, credentials_file)
-#
+
+for item in sectors.items():
+    sector_number = item[0]
+    sector_link = item[1]
+
+    print(f'Sector number: {sector_number}, sector link: {sector_link}')
 
