51304e2d0eceae42b6d94fae3c27a0469ac874c6
Andrey Mihaylov <trackclimb@gmail.com>
2024-05-14 16:45:12 +0300

    Refactoring

--- google_sheets/google_sheets_saver.py
+++ google_sheets/google_sheets_saver.py
@@ -8,7 +8,6 @@ class GoogleSheetsWorker:
         self.scopes = ["https://www.googleapis.com/auth/spreadsheets"]
         self.credentials_file = 'creds/google_creds.json'
 
-    # todo remove code duplication
     def clear_sheet(self, sheet_id, credentials_file, sheet_name):
         creds = Credentials.from_service_account_file(credentials_file, scopes=self.scopes)
         client = gspread.authorize(creds)
@@ -31,6 +30,9 @@ class GoogleSheetsWorker:
             sheet = workbook.add_worksheet(sheet_name, rows=len(ads_list) + 1, cols=15)
 
         sheet.clear()
+        self.add_ads_first_time(ads_list, sheet)
+
+    def add_ads_first_time(self, ads_list, sheet):
         ads_records = []
         self.add_headers(ads_records)
         self.get_records_from_ads_to_gs(ads_list, ads_records)
@@ -46,7 +48,7 @@ class GoogleSheetsWorker:
         if sheet_name in worksheet_list:
             sheet = workbook.worksheet(sheet_name)
             ads_records = []
-
+            # it could be old sheet without headers. Need to check this situation.
             values = sheet.row_values(1)
             rows_count = len(values)
             if rows_count == 0:
@@ -56,11 +58,7 @@ class GoogleSheetsWorker:
             sheet.append_rows(ads_records)
         else:
             sheet = workbook.add_worksheet(sheet_name, rows=len(ads_list) + 1, cols=15)
-            ads_records = []
-            self.add_headers(ads_records)
-            self.get_records_from_ads_to_gs(ads_list, ads_records)
-            sheet.update(range_name=f"A1:O{len(ads_records)}", values=ads_records)
-            sheet.format('A1:O1', {'textFormat': {'bold': True}})
+            self.add_ads_first_time(ads_list, sheet)
 
     def get_records_from_ads_to_gs(self, ads_list, ads_records):
         for ads in ads_list:
